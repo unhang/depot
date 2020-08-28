d8e0ff7680afe3f13d4372b22681b2ac63f93356
Ung Nhat Hang <hang.ung@executionlab.asia>
2020-08-28 21:11:10 +0700

    D1 + D2

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -11,6 +11,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     }
   end
 
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two)) 
+    end
+
+    assert_redirected_to products_url
+  end
+
   test "should get index" do
     get products_url
     assert_response :success
