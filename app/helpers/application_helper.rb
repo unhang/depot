b5625c93eebcd09d20c5ea4eb0c316b9910183f7
Ung Nhat Hang <hang.ung@executionlab.asia>
2020-08-30 15:29:06 +0700

    F4 hide empty cart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def hidden_div_if(condition, attributes = {}, &given_block)
+    if condition
+      attributes["style"] = "display: none"
+    end
+    content_tag "div", attributes, &given_block
+  end 
 end
