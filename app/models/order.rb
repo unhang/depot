c1d2fd6c03c1296a2a53b722157d0ef11dc19f0a
Ung Nhat Hang <hang.ung@executionlab.asia>
2020-09-03 17:42:35 +0700

    G1

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,9 +4,8 @@ class Order < ApplicationRecord
     'Credit card' => 1,
     'Purchase order' => 2
   }
-  
+
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: pay_types.keys
-
 
 end
