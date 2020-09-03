0c2ce3842de55f90c7cba013404e9327d6a5445c
Ung Nhat Hang <hang.ung@executionlab.asia>
2020-09-03 17:35:25 +0700

    G1

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,12 @@
 class Order < ApplicationRecord
-
-  validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys 
-
   enum pay_type: {
     'Check' => 0,
     'Credit card' => 1,
     'Purchase order' => 2
   }
+  
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
+
 end
